885a5c0634ef8cc8acb0b4fb622f380c717f1a78
Nguyen Tho Tuan <38480503+1712878@users.noreply.github.com>
2019-01-08 12:28:31 +0700

    BUILD CLASS MYDATE

---
+++ 1712878/LIBRARY/MyDate.cpp
@@ -0,0 +1,92 @@
+#include "MyDate.h"
+
+MyDate::MyDate(int day, int month, int year)
+{
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+
+
+bool MyDate::CheckDay()
+{
+	if (day <1 || day>DayOfMonth(month,year) || CheckYear(year) == -1)
+		return false;
+	else
+		return true;
+}
+
+bool MyDate::operator==(const MyDate & Date)
+{
+	return (day == Date.day && month == Date.month && year == Date.year);
+}
+
+MyDate MyDate::operator+ (int n)
+{	
+
+	int nDay = n  + day;
+	while (nDay > DayOfMonth(month,year))
+	{
+		nDay -= DayOfMonth(month,year);
+		if (month + 1 <= 12)
+			month++;
+		else
+		{
+			month = 1;
+			year++;
+		}
+	}
+	day = nDay;
+	return *this;
+}
+
+int MyDate::Distance()
+{
+	// 1/1/1900 -> this
+	int nDay = 0;
+	for (int i = MinYear; i < year; i++)
+		nDay += 365 + (CheckYear(i) == 1);
+	for (int i = 1; i < month; i++)
+		nDay += DayOfMonth(i, year);
+	nDay += day - 1;
+	return nDay;
+}
+
+int MyDate::Distance(MyDate Date)
+{
+	int nDay = Distance() - Date.Distance();
+	return abs(nDay);
+}
+
+string MyDate::DayOfWeek()
+{
+	string Day[] = { "MonDay", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday", "Sunday" };
+	int n = Distance() % 7; // 1/1/1900 -> Monday
+	return Day[n];
+}
+
+int CheckYear(int year)
+{
+	if (year < MinYear || year > MaxYear)
+		return -1;
+	else
+		return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+
+int DayOfMonth(int month, int year)
+{
+	if (month < 1 || month >12 || CheckYear(year) == -1)
+		return -1;
+	switch (month)
+	{
+	case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+		return 31; break;
+	case 4: case 6: case 9: case 11:
+		return 30; break;
+	default:
+		if (CheckYear(year) == 1)
+			return 29;
+		else
+			return 28;
+	}
+}
---
+++ 1712878/LIBRARY/MyDate.h
@@ -0,0 +1,37 @@
+#ifndef __MY_DATE_H__
+#define __MY_DATE_H__
+
+#include <iostream>
+#include <string>
+using namespace std;
+#define MinYear 1900
+#define	MaxYear 10000
+
+class MyDate
+{
+private:
+	int day, month, year;
+public:
+	MyDate(int day= 1, int month =1, int year = MinYear);
+	bool CheckDay();
+	bool operator == (const MyDate& Date);
+	MyDate operator+ (int nDay);
+	int Distance();
+	int Distance(MyDate Date);
+	string DayOfWeek();
+	friend istream& operator >> (istream& inDev, MyDate &Date)
+	{
+		char ch;
+		inDev >> Date.day >> ch >> Date.month >> ch >> Date.year;
+		return inDev;
+	}
+	friend ostream& operator << (ostream& outDev,const MyDate& Date)
+	{
+		outDev << Date.day << "/" << Date.month << "/" << Date.year;
+		return outDev;
+	}
+};
+int CheckYear(int year);
+int DayOfMonth(int month, int year);
+#endif
+
